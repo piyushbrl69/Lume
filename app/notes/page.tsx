33e0fa451cbce3425c1aed2ee42a3dd0868946d8
piyushbrl69 <piyushbrl100@gmail.com>
2026-08-18 13:18:13 +0530

    Notes rending fixed

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,10 @@
-import {Metadata} from "next";
+import { Metadata } from "next";
 import NotesClient from "./NotesClient";
+
 export const metadata: Metadata = {
   title: "Notes",
-}
-export default function HOme(){
-  <NotesClient/>;
+};
+
+export default function Home() {
+  return <NotesClient />;
 }
